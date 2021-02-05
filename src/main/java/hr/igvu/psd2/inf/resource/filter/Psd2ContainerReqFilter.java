ce344d26ec39a7f5f48f1f618aa67730865e5998
ivukovic <ivukovic@host.docker.internal>
2021-02-05 22:46:54 +0100

    filter stubs

---
+++ src/main/java/hr/igvu/psd2/inf/resource/filter/Psd2ContainerReqFilter.java
@@ -0,0 +1,16 @@
+package hr.igvu.psd2.inf.resource.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;	
+
+public class Psd2ContainerReqFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext arg0) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/hr/igvu/psd2/inf/resource/filter/Psd2ContainerRespFilter.java
@@ -0,0 +1,15 @@
+package hr.igvu.psd2.inf.resource.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+public class Psd2ContainerRespFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext arg0, ContainerResponseContext arg1) throws IOException {
+		// TODO Auto-generated method stub
+	}	
+}
